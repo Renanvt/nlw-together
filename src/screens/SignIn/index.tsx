3189f5c2032bf1c4b68b11d1dc71e7d5efcaa630
Renanvt <jonatan_rvs@hotmail.com>
2021-06-21 12:23:24 -0300

    conceito de estados

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput } from 'react-native';
 
 import { styles } from './styles'
 export function SignIn() {
-  return (
-      //Conceito de propriedade
-    <View style={styles.container}>
-      <Text>
-        Hello World, NLW Together
-      </Text>
-    </View>
-  );
+    //[ nome_stado, funcao_atualizar_estado]
+    //useState('valor_inicial')
+    const [text, setText] = useState('Rodrigo');
+
+    return (
+        //Conceito de propriedade
+        <View style={styles.container}>
+            <Text>
+                Hello World, NLW Together
+            </Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={setText}
+                //onChangeText={(value) => console.log(value)} //Observa o textinput e toda vez que um valor muda, mostra oque mudou
+            />
+            <Text>
+                Você digitou: ... {text}
+            </Text>
+        </View >
+    );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -3,6 +3,13 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         flex: 1, //Ocupa toda a tela
-        backgroundColor: 'red'
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    input: {
+        height: 50,
+        width: 200,
+        borderBottomWidth: 2
     }
 });
