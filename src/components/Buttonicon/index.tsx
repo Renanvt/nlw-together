b12a27b3baadaf62db8577fb17acb4171c033aca
Renanvt <jonatan_rvs@hotmail.com>
2021-06-21 14:21:58 -0300

    Props2

--- src/components/Buttonicon/index.tsx
+++ src/components/Buttonicon/index.tsx
@@ -4,21 +4,26 @@ import {
     Text,
     Image,
     View,
-    TouchableOpacity //Elemento de click
+    TouchableOpacity, //Elemento de click
+    TouchableOpacityProps
 } from 'react-native';
 
 import DiscordImg from '../../assets/discord.png'
 import { styles } from './styles';
 
 //interface Props{}
-type Props = {
+//Propriedades recebe todas as propriedades de TouchableOpacity e mais as propriedades definidas depois do colchetes
+type Props = TouchableOpacityProps & {
     //title?: string; //propriedade opcional
     title: string;
 
 }
-export function ButtonIcon({title}: Props) {
+export function ButtonIcon({title, activeOpacity}: Props) {
     return (
-        <TouchableOpacity style={styles.container}>
+        <TouchableOpacity 
+        style={styles.container}
+        activeOpacity={activeOpacity}
+        >
             <View style={styles.iconWrapper}>
                 <Image source={DiscordImg} style={styles.icon} />
             </View>
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -12,13 +12,13 @@ import { styles } from './styles'
 export function SignIn() {
     //[ nome_stado, funcao_atualizar_estado]
     //useState('valor_inicial')
-   
+
 
     return (
         //View é um elemento para criar grupos de componentes, para movimenta-los, organiza-los na interface
         <View style={styles.container}>
             <StatusBar
-            //Personaliza a barra de status
+                //Personaliza a barra de status
                 barStyle="light-content" //Deixa a barra de status como branca
                 backgroundColor="transparent"
                 translucent //conteúdo cola no limite da tela
@@ -26,14 +26,14 @@ export function SignIn() {
             <Image source={IllustrationImg}
                 style={styles.image}
                 resizeMode="stretch" //Garanti a imagem ajustada ao tamanho definido
-                />
+            />
 
             <View style={styles.content}>
                 <Text style={styles.title}>
                     Organize {`\n`}
                     suas jogatinas {`\n`}
                     facilmente
-                    
+
                 </Text>
 
                 <Text style={styles.subtitle}>
@@ -41,7 +41,9 @@ export function SignIn() {
                     favoritos com seus amigos
                 </Text>
 
-                <ButtonIcon title="Entrar com Discord"/>
+                <ButtonIcon
+                    title="Entrar com Discord" 
+                    activeOpacity={0.7}/>
             </View>
         </View>
     );
