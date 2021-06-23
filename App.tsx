a6b10499e681f57caafd3a4a325edc474faeb691
Renanvt <jonatan_rvs@hotmail.com>
2021-06-23 16:38:18 -0300

    install fonts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+
+import { Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+import { Rajdhani_500Medium, Rajdhani_700Bold } from '@expo-google-fonts/rajdhani';
+import { useFonts } from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ //Carrega as fontes
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold
+  })
+
+  if (!fontsLoaded) {
+    //Enquanto as fontes não carregarem, deixa a tela de splash em foco
+    return <AppLoading /> //Segura a tela de splash
+  }
   return (
     <SignIn />
   );
