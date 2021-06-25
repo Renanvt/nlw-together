45ddea9f0ad5be0d3d836b71be2b94439e110180
Renanvt <jonatan_rvs@hotmail.com>
2021-06-24 22:08:35 -0300

    adding and creating button add

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {View} from 'react-native';
 import { styles } from './styles';
+import { Profile } from '../../components/Profile';
+import { ButtonAdd } from '../../components/Profile/ButtonAdd';
 export function Home() {
     return (
         <View>
             <View style={styles.header}>
-
+                <Profile/>
+                <ButtonAdd />
             </View>
         </View>
     );
